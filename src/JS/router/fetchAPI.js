214068567a5077236cfdf21ce835d92dad942068
almeidadylan <almeidadylan793@gmail.com>
2022-05-16 21:13:59 -0400

    replacing the kenzie food api

--- src/JS/router/fetchAPI.js
+++ src/JS/router/fetchAPI.js
@@ -1,9 +1,10 @@
 class FetchProdutos{
     static async buscandoProdutos(){
         let array = []
-        let response = await fetch('https://kenzie-food-api.herokuapp.com/product')
+        let response = await fetch('https://kenzie-food-typescript.herokuapp.com/products')
         let dados = await response.json()
         .then(dados =>{
+            console.log(dados)
             dados.forEach(element =>{
                 array.push(element)
             })
